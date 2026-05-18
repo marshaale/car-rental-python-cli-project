ac88992713b361942a2205435ef3a5013c819efb
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-18 19:07:47 +0300

    implement car attributes and methods

--- app/model/car.py
+++ app/model/car.py
@@ -1,2 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
 class Car:
-    pass
+    id:int
+    name:str
+    model:str
+    year:str
+    price_per_day:float
+    is_rented:bool
+
+    def __str__(self):
+        return f"Name: {self.name}\nModel: {self.model}\nYear: {self.year}\nAvailable: {"No" if self.is_rented else "Yes"}\nDaily Price: ${self.price_per_day}"
