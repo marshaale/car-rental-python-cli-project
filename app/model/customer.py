82d89710b2eb322c5e720ca72c90d38f88bed6f4
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-19 20:19:28 +0300

    implement customer attributes and methods

--- app/model/customer.py
+++ app/model/customer.py
@@ -1,2 +1,11 @@
+from dataclasses import dataclass
+
+@dataclass
 class Customer:
-    pass
+    id:int
+    name:str
+    phone:str
+    identity:str # eg passport,national identity and more others 
+
+    def __str__(self) -> str:
+        return f"ID: {self.id}\nName: {self.name}\nPhone: {self.phone}\nIdentity: {self.identity}"
