0fcecf25188993fe3e6dbf6025fe24fa80b69d72
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-19 21:36:07 +0300

    Add customers  storage file bath

--- app/core/config.py
+++ app/core/config.py
@@ -3,3 +3,4 @@ from pathlib import Path
 ROOT = Path(__file__).parent.parent
 DATA_PATH = f"{ROOT}/data"
 CARS_FILE_PATH = f"{DATA_PATH}/cars.txt"
+CUSTOMERS_FILE_PATH = f"{DATA_PATH}/customers.txt"
