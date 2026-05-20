55f55aea9a593aadce03abc9d8e8b6860c4ce8ae
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-20 21:25:55 +0300

    call and load car rent service in system panel & test

--- app/main.py
+++ app/main.py
@@ -19,14 +19,25 @@ customer = Customer(id=1,name="Osman",phone="2368102",identity="National Identit
 # system.customer_service.add_customer(name="Osman",phone="2368102",identity="National Identity")
 # system.customer_service.add_customer(name="Ahmed",phone="760284",identity="Passport")
 
-print(system.customer_service.customers)
+# print(system.customer_service.customers)
 
-print(system.customer_service.find_customer(1))
+# print(system.customer_service.find_customer(1))
 
-print(system.customer_service.remove_customer(1))
+# print(system.customer_service.remove_customer(1))
 
-print(system.customer_service.update_customer(customer_id=2,identity="Document"))
+# print(system.customer_service.update_customer(customer_id=2,identity="Document"))
 
-print(system.customer_service.save_customers())
+# print(system.customer_service.save_customers())
 
-print(system.customer_service.customers)
+# print(system.customer_service.customers)
+
+system.car_rent_service.add_car_rent(car_id=1,customer_id=2,num_of_days=15,base_price=21,payment_type="Pre Payment")
+system.car_service.update_car(car_id=1,is_rented=True)
+system.car_rent_service.save_rent_cars()
+print(system.car_rent_service.customer_car_rents)
+print(system.car_rent_service.find_rent_car(1))
+system.car_service.update_car(car_id=1,is_rented=False)
+system.car_rent_service.update_rent_car(customer_car_rent_id=1,status="return_due_to_issue")
+print(system.car_rent_service.customer_car_rents)
+print(system.car_rent_service.remove_rent_car(2))
+system.car_rent_service.save_rent_cars()
--- app/model/system_panel.py
+++ app/model/system_panel.py
@@ -1,5 +1,6 @@
 from services.car_service import CarService
 from services.customer_service import CustomerService
+from services.customer_car_rent import CustomerCarRentService
 
 class SystemPanel:
     def __init__(self) -> None:
@@ -7,3 +8,5 @@ class SystemPanel:
         self.car_service.load_cars()
         self.customer_service = CustomerService()
         self.customer_service.load_customers()
+        self.car_rent_service = CustomerCarRentService()
+        self.car_rent_service.load_rent_cars()
