997b7923eb79ac8042a34ab0836e98d4183251af
marshaale <maxamedmuqtaaraxmed@gmail.com>
2026-05-23 21:15:46 +0300

    implement car information display

--- app/model/customer_car_rent.py
+++ app/model/customer_car_rent.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from services.customer_service import CustomerService
+from services.car_service import CarService
 
 @dataclass
 class CustomerCarRent:
@@ -9,3 +11,10 @@ class CustomerCarRent:
     base_price:float
     payment_type:str # pre payment or post payment
     status:str   # completed,return_due_to_issue,canceled
+
+    def display(self,car_service:CarService,customer_service:CustomerService) -> str:
+        car = car_service.find_car(car_id=self.car_id)
+        car_name = self.car_id if not car else car.name 
+        customer = customer_service.find_customer(customer_id=self.customer_id)
+        customer_name= self.customer_id if not customer else customer.name
+        return f"ID: {self.id}\nCar: {car_name}\nCustomer: {customer_name}\nRent days: {self.num_of_days}\nDaily Price: ${self.base_price}\nPrice to pay: ${self.num_of_days * self.base_price}"
